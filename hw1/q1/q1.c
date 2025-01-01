71364693ca112eca7e7cb9762731d8cb6f3b7754
Will Howe <willhowe@macbook-pro.mynetworksettings.com>
2025-01-01 16:31:31 -0500

    comment

--- hw1/q1/q1.c
+++ hw1/q1/q1.c
@@ -2,16 +2,34 @@
 #include <unistd.h>
 #include "q1.h"
 
-// Function definition
+/**
+ * Fork function with an integer local variable.
+ */
 void fork_function(void) {
+  // Start value of x at 10.
   int x = 10;
   printf("Value of x is: %d in process: %d\n", x, getpid());
+
+  // Fork the current process.
   fork();
+
+  // Value of x in parent process should be 10. Value of x in 
+  // child process is also 10.
   printf("Value of x is: %d in process: %d\n", x, getpid());
+
+  // Now set the value of 'x' to 20.
   x = 20;
+
+  // Observe that the value of x is changed to 20 in both the 
+  // child and parent processes. 
   printf("Value of x is: %d in process: %d\n", x, getpid());
 }
 
+/**
+ * Crucial takeaway: upon fork, the child process has its own
+ * address space so while it may appear like the processes are
+ * sharing the variable x, they each have their own "copy".
+ */
 int main() {
     // Call the function to fork your process
     fork_function();
