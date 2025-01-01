9b06501b3d8fc1a5d5f2958d46cf839000bb42fa
Will Howe <willhowe@macbook-pro.mynetworksettings.com>
2025-01-01 15:49:02 -0500

    init comit

---
+++ hw1/q1/q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "q1.h"
+
+// Function definition
+void print_hello_world(void) {
+    printf("Hello, World!\n");
+}
+
+int main() {
+    // Call the function to print "Hello, World!"
+    print_hello_world();
+    return 0;
+}
---
+++ hw1/q1/q1.h
@@ -0,0 +1,6 @@
+#ifndef Q1_H
+#define Q1_H
+
+void print_hello_world(void);
+
+#endif // Q1_H
