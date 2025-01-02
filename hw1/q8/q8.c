f270f1aaa9046ca51d9baec92d16d718e9565ee9
Will Howe <willhowe@macbook-pro.mynetworksettings.com>
2025-01-01 19:26:09 -0500

    q8

---
+++ hw1/q8/q8.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "q8.h"
+
+/**
+ * Fork function.
+ */
+void fork_function(void) {
+  // Grab parent pid.
+  pid_t tParent = getpid();
+
+  // Fork the current process.
+  fork();
+
+  if (getpid() == tParent)
+  {
+    printf("goodbye\n");
+  }
+  else
+  {
+    close(STDOUT_FILENO);
+    printf("hello\n");
+  }
+}
+
+/**
+ * After closing the STDOUT_FILENO the child process is not longer 
+ * able to print to stdout, but the parent process is still able
+ * to do so.
+ */
+int main() {
+    // Call the function to fork your process
+    fork_function();
+    return 0;
+}
