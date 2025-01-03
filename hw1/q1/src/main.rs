bead0c5f7de87f0a2285f69d51208e633400633f
Will Howe <willhowe@macbook-pro.mynetworksettings.com>
2025-01-03 14:11:53 -0500

    q1

--- hw1/q1/src/main.rs
+++ hw1/q1/src/main.rs
@@ -1,3 +1,36 @@
+use nix::unistd::{fork, ForkResult};
+use std::process::exit;
+
+unsafe fn fork_function() {
+    // Start the value of x out at 10.
+    let mut x = 10;
+    println!("Value of x is: {} in process: {}", x, std::process::id());
+
+    // Fork the current process
+    match fork() {
+        Ok(ForkResult::Parent { .. }) => {
+            println!("Value of x is: {} in the parent.", x);
+            x = 20;
+            println!("Value of x is {} in the parent.", x);
+        }
+        Ok(ForkResult::Child) => {
+            println!("Value of x is {} in the child.", x);
+
+            x = 30;
+            println!("Value of x is {} in the child.", x);
+
+            exit(0);  // Child exits here
+
+        }
+        Err(e) => {
+            eprintln!("Fork failed: {}", e);
+            exit(1);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        fork_function();
+    }
 }
