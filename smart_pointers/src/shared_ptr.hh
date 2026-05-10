f3c3f052e8bf8f5a7e607803c37a2f699b7869ae
Will Howe <willhowe235@gmail.com>
2026-05-10 19:01:18 -0400

    shared_ptr | finished for now

--- smart_pointers/src/shared_ptr.hh
+++ smart_pointers/src/shared_ptr.hh
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <cassert>
+#include <atomic>
 
 struct ControlBlock {
-    int ref_count;
+    std::atomic<int> ref_count;
+    ControlBlock(int ref_count) 
+        :
+            ref_count(ref_count)
+    {
+    }
 };
 
 template<typename T>
@@ -16,10 +22,8 @@ public:
     SharedPtr(T* raw_ptr)
         :
             raw_ptr(raw_ptr),
-            control_block(new ControlBlock())
+            control_block(new ControlBlock(1))
     {
-        // Start the reference count at 1.
-        this->control_block->ref_count = 1;
     }
 
     // Destructor
@@ -47,22 +51,31 @@ public:
     SharedPtr& operator=(const SharedPtr& other) {
         // Prevent self-assignment
         if (this != &other) {
+            // If the object we are assigning to exists and is the last reference
+            // then clean it up.
+            if (this->control_block && --this->control_block->ref_count == 0) {
+                delete this->raw_ptr;
+                delete this->control_block;
+            }
+
             this->raw_ptr = other.raw_ptr;
             this->control_block = other.control_block;
-            ++this->control_block->ref_count;
+            // If the other object wasn't a nullptr, increment the ref count.
+            if (control_block) ++this->control_block->ref_count;
         }
         return *this;
     }
 
     // Copy Ctor
-    SharedPtr(const SharedPtr& other) noexcept {
+    SharedPtr(const SharedPtr& other) {
         this->raw_ptr = other.raw_ptr;
         this->control_block = other.control_block;
-        ++this->control_block->ref_count;
+        // If the other object wasn't a nullptr, increment the ref count.
+        if (control_block) ++this->control_block->ref_count;
     }
 
     // Move Assignment
-    SharedPtr& operator=(SharedPtr&& other) {
+    SharedPtr& operator=(SharedPtr&& other) noexcept {
         if (this != &other) {
 
             // Release current SharedPtr's resources
@@ -101,6 +114,45 @@ public:
 
     explicit operator bool() const {
         return this->raw_ptr != nullptr;
+    }
+
+    // Arrow Operator
+    T* operator->() {
+        return this->raw_ptr;
+    }
+
+    // Const Arrow Operator
+    const T* operator->() const {
+        return this->raw_ptr;
+    }
+
+    T* get() const {
+        return this->raw_ptr;
+    }
+
+    T* release() {
+        auto temp = this->raw_ptr;
+
+        if (this->control_block && --this->control_block->ref_count == 0) {
+            delete this->control_block;
+        }
+
+        // Null out this object
+        this->control_block = nullptr;
+        this->raw_ptr = nullptr;
+
+        return temp;
+    }
+
+    void reset(T* new_ptr = nullptr) {
+        if (this->raw_ptr != new_ptr) {
+            if (this->control_block && --this->control_block->ref_count == 0) {
+                delete this->raw_ptr;
+                delete this->control_block;
+            }
+            this->raw_ptr = new_ptr;
+            this->control_block = new_ptr ? new ControlBlock(1) : nullptr;
+        }
     }
 
 private:
