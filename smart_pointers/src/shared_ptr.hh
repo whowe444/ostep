fd724c4117f391f9e35d7b4ab742ecc0471673a7
Will Howe <willhowe235@gmail.com>
2026-05-10 18:41:05 -0400

    shared_ptr | rule of 5

--- smart_pointers/src/shared_ptr.hh
+++ smart_pointers/src/shared_ptr.hh
@@ -18,22 +18,27 @@ public:
             raw_ptr(raw_ptr),
             control_block(new ControlBlock())
     {
-        control_block->ref_count = 0;
+        // Start the reference count at 1.
+        this->control_block->ref_count = 1;
     }
 
     // Destructor
     ~SharedPtr() {
         // Delete raw_ptr if reference count hits zero
-        if (this->control_block && this->control_block->ref_count == 0) {
+        if (this->control_block && --this->control_block->ref_count == 0) {
             delete this->raw_ptr;
             delete this->control_block;
-        } else if (this->control_block) {
-            --this->control_block->ref_count;
         }
     }
 
     // Non-Const Dereference Operator
     T& operator*() {
+        assert(this->raw_ptr && "Dereferencing null SharedPtr!");
+        return *this->raw_ptr;
+    }
+
+    // Const Dereference Operator
+    const T& operator*() const {
         assert(this->raw_ptr && "Dereferencing null SharedPtr!");
         return *this->raw_ptr;
     }
@@ -46,6 +51,7 @@ public:
             this->control_block = other.control_block;
             ++this->control_block->ref_count;
         }
+        return *this;
     }
 
     // Copy Ctor
@@ -55,12 +61,46 @@ public:
         ++this->control_block->ref_count;
     }
 
+    // Move Assignment
+    SharedPtr& operator=(SharedPtr&& other) {
+        if (this != &other) {
+
+            // Release current SharedPtr's resources
+            if (this->control_block && --this->control_block->ref_count == 0) {
+                delete this->raw_ptr;
+                delete this->control_block;
+            }
+
+            this->raw_ptr = other.raw_ptr;
+            other.raw_ptr = nullptr;
+            this->control_block = other.control_block;
+            other.control_block = nullptr;
+        }
+        // Don't modify the reference count since we are going from
+        // two things down to one, and we already decremented above.
+        return *this;
+    }
+
+    // Move Constructor
+    SharedPtr(SharedPtr&& other) noexcept 
+        :
+            raw_ptr(other.raw_ptr),
+            control_block(other.control_block)
+    {
+        other.raw_ptr = nullptr;
+        other.control_block = nullptr;
+    }
+
     bool operator==(const SharedPtr& other) const {
         return this->raw_ptr == other.raw_ptr;
     }
 
     bool operator!=(const SharedPtr& other) const {
         return !(*this == other);
+    }
+
+    explicit operator bool() const {
+        return this->raw_ptr != nullptr;
     }
 
 private:
--- smart_pointers/test/shared_ptr_test.cc
+++ smart_pointers/test/shared_ptr_test.cc
@@ -1,15 +1,44 @@
 #include "shared_ptr_test.hh"
 
 TEST_F(SharedPtrTest, TestConstructor) {
+    EXPECT_TRUE(my_shared_ptr);
     EXPECT_EQ(*my_shared_ptr, 5);
 }
 
 TEST_F(SharedPtrTest, TestCopyAssignment) {
-    SharedPtr<int> new_shared_ptr = my_shared_ptr;
+    SharedPtr<int> new_shared_ptr = SharedPtr<int>(new int(10));
+    new_shared_ptr = my_shared_ptr;
     EXPECT_EQ(new_shared_ptr, my_shared_ptr);
 }
 
 TEST_F(SharedPtrTest, TestCopyConstructor) {
     SharedPtr<int> new_shared_ptr (my_shared_ptr);
     EXPECT_EQ(new_shared_ptr, my_shared_ptr);
+}
+
+TEST_F(SharedPtrTest, TestDestructor) {
+    int* my_int = new int(4);
+
+    // Construct the object within this scope
+    {
+        SharedPtr<int> new_shared_ptr;
+        new_shared_ptr = SharedPtr<int>(my_int);
+    }
+
+    // Since my_int has be deleted, the data will be garbage.
+    EXPECT_NE(*my_int, 4);
+}
+
+TEST_F(SharedPtrTest, TestMoveAssignment) {
+    SharedPtr<int> new_shared_ptr = SharedPtr<int>(new int(10));
+    new_shared_ptr = std::move(my_shared_ptr);
+
+    EXPECT_TRUE(new_shared_ptr);
+    EXPECT_FALSE(my_shared_ptr);
+}
+
+TEST_F(SharedPtrTest, TestMoveConstructor) {
+    SharedPtr<int> new_shared_ptr = std::move(my_shared_ptr);
+    EXPECT_TRUE(new_shared_ptr);
+    EXPECT_FALSE(my_shared_ptr);
 }
